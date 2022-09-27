96fc0e83bbdc8503b0bba574b1b5c46ad04c806d
andrieu <pierre.andrieu@lri.fr>
2022-09-27 22:39:54 +0200

    first test on initialisation

---
+++ tests.py
@@ -0,0 +1,23 @@
+from jdlv.jdlv import *
+
+
+def tests_initialisation():
+    def test_empty_constructor():
+        try:
+            jdlv = Jdlv()
+        except TypeError:
+            pass
+
+    test_empty_constructor()
+
+
+def tests_mise_a_jour():
+    pass
+
+
+def tests_nombre_cellules():
+    pass
+
+
+tests_initialisation()
+
