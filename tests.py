e5d9800a8431fe97db34d69ac43c482fc2b24325
andrieu <pierre.andrieu@lri.fr>
2022-09-27 23:04:07 +0200

    first test that currently fails: Jdlv constructor asserts one argument

--- tests.py
+++ tests.py
@@ -5,17 +5,18 @@ def tests_initialisation():
     def test_empty_constructor():
         try:
             jdlv = Jdlv()
+            raise Exception("Le constructeur de Jdlv ne doit pas autoriser un constructeur vide")
+
         except TypeError:
             pass
-
     test_empty_constructor()
 
 
-def tests_mise_a_jour():
+def tests_nombre_cellules():
     pass
 
 
-def tests_nombre_cellules():
+def tests_mise_a_jour():
     pass
 
 
